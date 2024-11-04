959253bd13764ea25a456a007abb1105533399f6
yd1107 <tomoko1107.ky413@gmail.com>
2024-11-04 22:42:20 +0900

    11.4レッスン

--- restaurant/views.py
+++ restaurant/views.py
@@ -68,32 +68,6 @@ class TermsView(generic.TemplateView):
     template_name = "layout/terms.html"
 
 
-class TopPageView(generic.ListView):
-    """ トップ画面=================================="""
-    template_name = "top_page.html"
-    model = models.Restaurant
-    queryset = models.Restaurant.objects.order_by('-rate')
-    context_object_name = 'restaurant_list'
-
-    def get_context_data(self, **kwargs):
-        if 'price_session' in self.request.session:
-            self.request.session['price_session'] = 0
-        if 'keyword_session' in self.request.session:
-            self.request.session['keyword_session'] =''
-        if 'category_session' in self.request.session:
-            self.request.session['category_session'] =''
-        if 'select_sort' in self.request.session:
-            self.request.session['select_sort'] = '-created_at'
-        context = super(TopPageView, self).get_context_data(**kwargs)
-        category_list = models.Category.objects.all()
-        new_restaurant_list = models.Restaurant.objects.all().order_by('-created_at')
-
-        context.update({
-            'category_list': category_list,
-            'new_restaurant_list': new_restaurant_list,
-        })
-        return context
-
 class RestaurantDetailView(generic.DetailView):
     """ レストラン詳細画面=================================="""
     template_name = "restaurant/restaurant_detail.html"
@@ -401,6 +375,7 @@ def reservation_delete(request):
             is_success = False
     else:
         is_success = False
+
     return JsonResponse({'is_success': is_success})
 
 class ReviewListView(generic.ListView):
