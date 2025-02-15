991d12d7ba8a84e6edf2d15d7813d29ef3d2b672
yd1107 <tomoko1107.ky413@gmail.com>
2025-02-15 23:55:38 +0900

    2.15レビュー関連りファクタリング

--- restaurant/views.py
+++ restaurant/views.py
@@ -152,7 +152,6 @@ class RestaurantSearchView(generic.TemplateView):
         # URLクエリパラメータ取得
         search_type = self.request.GET.get("search_type")       # 検索種類
 
-        print(f"search_type:{search_type}")
 
         if search_type is not None:     # 検索条件指定の場合 セッション情報クリア
             _session["keyword"] = ""
@@ -176,7 +175,6 @@ class RestaurantSearchView(generic.TemplateView):
             else:
                 search_type = "keyword"
         
-        print(f"search_type:{search_type}")
 
         # ソート情報でない場合 検索条件の内容を該当セッション変数に保存
         restaurant_list = []
@@ -197,6 +195,12 @@ class RestaurantSearchView(generic.TemplateView):
         restaurant_list = models.Restaurant.objects.filter(condition)
         restaurant_list = restaurant_list.order_by("-created_at")
 
+        for restaurant in restaurant_list:
+            review_obj = models.Review.objects.filter(restaurant=restaurant).aggregate(Avg('rate'))
+            restaunrat_rate = review_obj['rate__avg'] if review_obj['rate__avg'] is not None else 0
+
+            restaurant.review_num = models.Review.objects.filter(restaurant=restaurant).count()
+
         context = {
             "keyword": _session["keyword"],
             "category_selected": _session["category"],
@@ -209,117 +213,6 @@ class RestaurantSearchView(generic.TemplateView):
 
         return context
 
-
-    '''
-    model = models.Restaurant
-
-    def get_context_data(self, **kwargs):
-        context = super(RestaurantListView, self).get_context_data(**kwargs)
-        
-        # get input value
-        keyword = self.request.GET.get('keyword')
-        category = self.request.GET.get('category')
-        price_min = self.request.GET.get('price_min')
-        price_max = self.request.GET.get('price_max')
-        select_sort = self.request.GET.get('select_sort')
-        button_type = self.request.GET.get('button_type')
-
-        keyword = keyword if keyword is not None else ''
-        category = category if category is not None else ''
-        price_min = price_min if price_min is not None else 0
-        price_max = price_max if price_max is not None else 1000000
-        select_sort = select_sort if select_sort is not None else '-created_at'
-
-    # session control
-        self.request.session['select_sort'] = select_sort
-
-        if button_type == 'keyword':
-            self.request.session['keyword_session'] = keyword
-            self.request.session['category_session'] = ''
-            self.request.session['price_min_session'] = 0
-            self.request.session['price_max_session'] = 0
-        
-        if button_type == 'category':
-            self.request.session['category_session'] = category
-            self.request.session['keyword_session'] =''
-            self.request.session['price_min_session'] = 0
-            self.request.session['price_max_session'] = 0
-            
-        if button_type == 'price':
-            self.request.session['price_min_session'] = price_min
-            self.request.session['price_max_session'] = price_max
-            self.request.session['keyword_session'] =''
-            self.request.session['category_session'] =''
-
-        if button_type == 'select_sort':
-            self.request.session['select_sort'] = select_sort
-
-        keyword_session = self.request.session.get('keyword_session')
-        category_session = self.request.session.get('category_session')
-        price_min_session = int(self.request.session.get('price_min_session'))
-        price_max_session = int(self.request.session.get('price_max_session'))
-        select_sort_session = self.request.session.get('select_sort')
-
-        # filtering queryset
-        restaurant_list = models.Restaurant.objects.filter(Q(name__icontains=keyword_session)|Q(address__icontains=keyword_session)|Q(category__name__icontains=keyword_session))
-        restaurant_list = restaurant_list.filter(category__name__icontains=category_session)
-
-        if price_min_session > 0 or price_max_session > 0:
-            restaurant_data = models.Restaurant.objects.values('id', 'price_min', 'price_max')
-            target_id_list = []
-
-            for data in restaurant_data:
-                if data["price_min"] >= price_min_session and data["price_max"] <= price_max_session:
-                    target_id_list.append(data['id'])
-
-                
-                price_str = data['price']
-                price_str = price_str.replace('円','')
-                price_str = price_str.replace(',','')
-                price_list = price_str.split('～')
-
-                if int(price_list[0]) <= int(price_session) <= int(price_list[1]):
-                    target_id_list.append(data['id'])
-
-                 
-            restaurant_list = restaurant_list.filter(id__in=target_id_list)
-
-        # 表示順
-        restaurant_list = restaurant_list.order_by(select_sort_session)
-        
-        category_list = models.Category.objects.all()
-
-        # querysetに含まれるレストランの平均レートを、レストランごとに取得して配列に格納
-        average_rate_list = []
-        average_rate_star_list = []
-        rate_num_list = []
-
-        for restaurant in restaurant_list:
-            average_rate = models.Review.objects.filter(restaurant=restaurant).aggregate(Avg('rate'))
-            average_rate = average_rate['rate__avg'] if average_rate['rate__avg'] is not None else 0 
-            average_rate_list.append(round(average_rate, 2))
-            
-            if average_rate % 1 == 0:
-                average_rate = int(average_rate)
-            else:
-                average_rate = round(average_rate * 2) / 2
-            average_rate_star_list.append(average_rate)
-            
-            rate_num = models.Review.objects.filter(restaurant=restaurant).count()
-            rate_num_list.append(rate_num)
-
-        context.update({
-            'category_list': category_list,
-            'keyword_session': keyword_session,
-            'category_session': category_session,
-            'price_min_session': price_min_session,
-            'price_max_session': price_max_session,
-            'select_sort_session': select_sort_session,
-            'restaurant_list': zip(restaurant_list, average_rate_list,average_rate_star_list, rate_num_list),
-         })
-        return context
-
-'''
 
 class FavoriteListView(generic.ListView):
     """ お気に入り一覧画面=================================="""
