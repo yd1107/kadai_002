2e536befba787527a54b29883e2c6b2b5a8ad1c5
yd1107 <tomoko1107.ky413@gmail.com>
2025-04-29 01:39:20 +0900

    新規登録画面サブスクチェック削除

--- accounts/forms.py
+++ accounts/forms.py
@@ -50,7 +50,7 @@ class MySignupForm(SignupForm):
     zip_code = forms.CharField(max_length=255, label='郵便番号')
     address = forms.CharField(max_length=255, label='住所')
     phone_number = forms.CharField(max_length=255, label='電話番号')
-    birthday = forms.CharField(max_length=255, label='誕生日')
+    birthday = forms.DateField(label='誕生日')
     job = forms.CharField(max_length=255, label='職業')
     is_subscribed = forms.BooleanField(required=False)
     
@@ -61,22 +61,26 @@ class MySignupForm(SignupForm):
       self.fields['zip_code'].widget = forms.TextInput(attrs={'placeholder': '1010022'})
       self.fields['address'].widget = forms.TextInput(attrs={'placeholder': '東京都千代田区神田棟堀町300番地'})
       self.fields['phone_number'].widget = forms.TextInput(attrs={'placeholder': '09012345678'})
-      self.fields['birthday'].widget = forms.TextInput(attrs={'placeholder': '19950401'})
+      self.fields['birthday'].widget = forms.DateInput(attrs={"type":"date", "min":"1955-01-01", "max":"2007-12-31"})
       self.fields['job'].widget = forms.TextInput(attrs={'placeholder': 'エンジニア'})
       self.fields['email'].widget = forms.TextInput(attrs={'type': 'email','placeholder':'taro.samurai@example.com'})
       self.fields['password1'].widget = forms.PasswordInput(attrs={'class':'form-control'})
       self.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control'})
+      self.fields["is_subscribed"].widget = forms.HiddenInput()
       
-      def signup(self, request, user):
-        user.user_name = self.cleaned_data['user_name']
-        user.hurigana = self.cleaned_data['hurigana']
-        user.zip_code = self.cleaned_data['zip_code']
-        user.address = self.cleaned_data['address']
-        user.phone_number = self.cleaned_data['phone_number']
-        user.birthday = self.cleaned_data['birthday']
-        user.job = self.cleaned_data['job']
-        user.save()
-        return user
+      self.label_suffix = ""
+      
+    def signup(self, request, user):
+      user.user_name = self.cleaned_data['user_name']
+      user.hurigana = self.cleaned_data['hurigana']
+      user.zip_code = self.cleaned_data['zip_code']
+      user.address = self.cleaned_data['address']
+      user.phone_number = self.cleaned_data['phone_number']
+      user.birthday = self.cleaned_data['birthday']
+      user.job = self.cleaned_data['job']
+      user.is_subscribed = False
+      user.save()
+      return user
 
 class MyLoginForm(LoginForm):
     def __init__(self, *args, **kwargs):
